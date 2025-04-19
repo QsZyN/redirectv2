efac8a5d29626ce14fcdd0f6cd1ee21943f9a776
QsZyN <149283132+QsZyN@users.noreply.github.com>
2025-04-19 04:04:30 +0000

    fix

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,7 @@
-let url = window.location.href.replace('https://qszyn.github.io/redirectv2/', '');
-console.log(url);
-// location.replace(url);
+var url = window.location.href.replace('https://qszyn.github.io/redirectv2', '');
+if (url === '' || url === '/') {
+    url = "https://github.com/QsZyN/redirectv2";  
+}else{
+    url = url.replace(/^\//, '');
+}
+location.replace(url);
